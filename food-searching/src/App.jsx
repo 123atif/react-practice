2075aa09b03e534a5bb49fb80e874b581f42bc74
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-12-27 23:10:55 +0500

    setup tailwind

--- food-searching/src/App.jsx
+++ food-searching/src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <header className="bg-blue-500 text-white p-4">
+          <h1 className="text-xl">Welcome to React with Tailwind CSS!</h1>
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default App;
