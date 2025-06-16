6a7cb77e2867533aa992261fab41b84d4c47547c
Atif Arshad <atifarshad0315@gmail.com>
2025-06-16 19:31:07 +0500

    make props data for practice

--- counter/vite-project/src/App.jsx
+++ counter/vite-project/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount(count + 1);
+  };
+
+  const decCOunt = () => {
+    if (count <= 0) {
+      alert("you are getting in -ve which is not allowed");
+    } else {
+      setCount(count - 1);
+    }
+  };
   return (
     <>
-      <h1>s</h1>
+      <h1> You count me {count}</h1>
+      <button onClick={decCOunt}>-</button>
+      <button onClick={addCount}>+</button>
     </>
   );
 }
