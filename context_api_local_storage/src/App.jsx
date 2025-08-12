26326dc0bd7857df2b575d7e9ae9bc4e39bbe8cb
Atif Arshad <81535521+123atif@users.noreply.github.com>
2025-08-13 00:59:07 +0500

    make todo with context and localstorage

--- context_api_local_storage/src/App.jsx
+++ context_api_local_storage/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import { TodoProvider } from "./context/todoContext";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -17,8 +19,20 @@ function App() {
   }
 
   const toogleComplete = (id) => {
-    setTodos((prev) => prev.map((toggleTodo) => (toggleTodo === id ? { ...toggleTodo, completed: !toggleTodo.completed } : toggleTodo)))
+    setTodos((prev) => prev.map((toggleTodo) => (toggleTodo.id === id ? { ...toggleTodo, completed: !toggleTodo.completed } : toggleTodo)))
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
 
   return (
     <TodoProvider value={{ todos, addTodo, deleteTodo, editTodo, toogleComplete }}>
@@ -26,10 +40,15 @@ function App() {
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
+            <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
